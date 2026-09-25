3dd6a4f8187f816aa64346fb8853645b12dc7298
Melisande Onana <melisande.onana@orange.fr>
2026-09-25 12:16:54 +0200

    ajout de nouveaux fichiers d'images et mise à jour des règles de réécriture dans .htaccess

--- src/router.php
+++ src/router.php
@@ -1,9 +1,30 @@
 <?php
 require 'utils/splAutoload.php';
 
-header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+	http_response_code(204);
+	exit;
+}
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if (str_starts_with($uri, '/assets/')) {
+	$assetsRoot = realpath(__DIR__ . '/assets');
+	$assetPath = realpath(__DIR__ . $uri);
+
+	if ($assetsRoot && $assetPath && str_starts_with($assetPath, $assetsRoot . DIRECTORY_SEPARATOR) && is_file($assetPath)) {
+		header('Content-Type: ' . (mime_content_type($assetPath) ?: 'application/octet-stream'));
+		readfile($assetPath);
+		exit;
+	}
+}
+
+header('Content-Type: application/json');
+
 $segments = array_values(array_filter(explode('/', $uri)));
 
 if (empty($segments)) {
